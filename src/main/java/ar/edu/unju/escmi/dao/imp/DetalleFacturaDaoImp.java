383887fb84ec696adbd902ca624c6e4b3b2093d5
orellanaariana <cococonpitula@gmail.com>
2025-11-07 22:32:00 -0300

    ArianaOrellana

--- src/main/java/ar/edu/unju/escmi/dao/imp/DetalleFacturaDaoImp.java
+++ src/main/java/ar/edu/unju/escmi/dao/imp/DetalleFacturaDaoImp.java
@@ -4,7 +4,6 @@ import java.util.List;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
-
 import ar.edu.unju.escmi.config.EmfSingleton;
 import ar.edu.unju.escmi.dao.IDetalleFacturaDao;
 import ar.edu.unju.escmi.entities.DetalleFactura;
@@ -27,13 +26,23 @@ public class DetalleFacturaDaoImp implements IDetalleFacturaDao {
         } catch (Exception e) {
             if (tx.isActive()) tx.rollback();
             e.printStackTrace();
+        } finally {
+            em.close();
         }
     }
 
     @Override
     public List<DetalleFactura> obtenerDetalles() {
+        TypedQuery<DetalleFactura> query =
+            em.createQuery("SELECT d FROM DetalleFactura d", DetalleFactura.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<DetalleFactura> obtenerDetallesPorFactura(Long idFactura) {
         TypedQuery<DetalleFactura> query = em.createQuery(
-            "SELECT d FROM DetalleFactura d", DetalleFactura.class);
+            "SELECT d FROM DetalleFactura d WHERE d.factura.id = :idFactura", DetalleFactura.class);
+        query.setParameter("idFactura", idFactura);
         return query.getResultList();
     }
 }
